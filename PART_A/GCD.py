a4dc24667c744c00a8f269752db731b21f7e0d65
BitRebels <122071878+mohd-shaamii@users.noreply.github.com>
2023-09-07 16:56:14 +0530

    Create GCD.py

---
+++ PART_A/GCD.py
@@ -0,0 +1,15 @@
+import math
+
+# Input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculate the GCD
+gcd = math.gcd(num1, num2)
+
+# Print the GCD
+print(f"The GCD of {num1} and {num2} is {gcd}")
+
+# Enter the first number: 24
+# Enter the second number: 30
+# The GCD of 24 and 30 is 6
